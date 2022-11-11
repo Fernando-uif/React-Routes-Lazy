4b3dde68da37e688872ffdb7f689c2ca76dd10b7
Fernando <fernando.sosa@grupobituaj.com.mx>
2022-11-11 16:15:10 -0600

    Add the functions of the shopping

--- src/02-components-patters/components/ProductCard.tsx
+++ src/02-components-patters/components/ProductCard.tsx
@@ -1,17 +1,25 @@
 import styles from "../styles/styles.module.css";
 import image from "../public/coffee-mug.png";
 import noImage from "../assets/no-image.jpg";
+import { useState } from "react";
 
 export const ProductCard = () => {
+  const [counter, setCounter] = useState(0);
+  const increaseBy = (value: number) => {
+    setCounter((prev) => Math.max(prev + value, 0));
+  };
+
   return (
     <>
       <div className={styles.productCard}>
         <img className={styles.productImg} src={noImage} alt="coffemug" />
         <span className={styles.productDescription}>Coffe Mug</span>
         <div className={styles.buttonsContainer}>
-          <button className={styles.buttonMinus}>-</button>
-          <div className={styles.countLabel}>0</div>
-          <button className={styles.buttonAdd}>+</button>
+          <button  onClick={() => increaseBy(-1)} className={styles.buttonMinus}>-</button>
+          <div className={styles.countLabel}>{counter}</div>
+          <button onClick={() => increaseBy(1)} className={styles.buttonAdd}>
+            +
+          </button>
         </div>
       </div>
     </>
