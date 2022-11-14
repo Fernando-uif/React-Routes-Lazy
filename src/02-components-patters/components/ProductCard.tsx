371cef06a6d5a5d53c6a8543881f4be84bc5d38f
Fernando <fernando.sosa@grupobituaj.com.mx>
2022-11-14 15:48:13 -0600

    Agrego componentes por partes

--- src/02-components-patters/components/ProductCard.tsx
+++ src/02-components-patters/components/ProductCard.tsx
@@ -1,26 +1,55 @@
 import styles from "../styles/styles.module.css";
 import image from "../public/coffee-mug.png";
 import noImage from "../assets/no-image.jpg";
-import { useState } from "react";
 import { useProduct } from "../hooks/useProduct";
+import { Product } from "../interfaces/Product";
 
-export const ProductCard = () => {
+interface Props {
+  product: Product;
+}
+
+export const ProductImage = ({ img = false }) => {
+  return (
+    <img
+      className={styles.productImg}
+      src={img ? image : noImage}
+      alt="coffemug"
+    />
+  );
+};
+export const ProductTitle = ({ title }: { title: string }) => {
+  return <span className={styles.productDescription}>{title}</span>;
+};
+
+export const ProductsButtons = ({
+  counter,
+  increaseBy,
+}: {
+  counter: number;
+  increaseBy: (arg:number) => void;
+}) => {
+  return (
+    <div className={styles.buttonsContainer}>
+      <button onClick={() => increaseBy(-1)} className={styles.buttonMinus}>
+        -
+      </button>
+      <div className={styles.countLabel}>{counter}</div>
+      <button onClick={() => increaseBy(1)} className={styles.buttonAdd}>
+        +
+      </button>
+    </div>
+  );
+};
+
+export const ProductCard = ({ product }: Props) => {
   const { counter, increaseBy } = useProduct();
 
   return (
     <>
       <div className={styles.productCard}>
-        <img className={styles.productImg} src={noImage} alt="coffemug" />
-        <span className={styles.productDescription}>Coffe Mug</span>
-        <div className={styles.buttonsContainer}>
-          <button onClick={() => increaseBy(-1)} className={styles.buttonMinus}>
-            -
-          </button>
-          <div className={styles.countLabel}>{counter}</div>
-          <button onClick={() => increaseBy(1)} className={styles.buttonAdd}>
-            +
-          </button>
-        </div>
+        <ProductImage img={product.img} />
+        <ProductTitle title={product.title} />
+        <ProductsButtons counter={counter} increaseBy={increaseBy} />
       </div>
     </>
   );
--- src/02-components-patters/pages/ShoppingPage.tsx
+++ src/02-components-patters/pages/ShoppingPage.tsx
@@ -1,13 +1,26 @@
 import React from "react";
 import { ProductCard } from "../components/ProductCard";
-
+import { Product } from "../interfaces/Product";
+const product: Product = {
+  id: "1",
+  title: "Coffe mub- card",
+  img: false,
+};
 export const ShoppingPage = () => {
   return (
     <>
       <div>
         ShoppingPage
         <hr />
-        <ProductCard />
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            flexWrap: "wrap",
+          }}
+        >
+          <ProductCard product={product} />
+        </div>
       </div>
     </>
   );
